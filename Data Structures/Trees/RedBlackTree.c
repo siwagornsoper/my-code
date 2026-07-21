7c843c20cc9109d0092ecad1f0eaba288a5ff554
Siwagorn Soper <siwagornsoper@gmail.com>
2026-07-20 23:45:52 -0400

    added treeRemove

--- Data Structures/Trees/RedBlackTree.c
+++ Data Structures/Trees/RedBlackTree.c
@@ -27,6 +27,23 @@ int treeEmpty(struct Tree *tree){
         return 0;
 }
 
+struct Node *treeSearch(struct Tree *tree, int num){
+    struct Node *i;
+
+    i = tree->root;
+
+    while(i != NULL){
+        if(i->item > num)
+            i = i->left;
+        else if(i->item < num)
+            i = i->right;
+        else
+            return i;
+    }
+
+    return NULL;
+}
+
 void treeInsert(struct Tree *tree, int num){
     struct Node *newNode;
     struct Node *i;
@@ -62,6 +79,64 @@ void treeInsert(struct Tree *tree, int num){
         parent->right = newNode;
 }
 
+struct Node *treeFindMin(struct Node *node){
+    struct Node *min;
+    
+    while(node != NULL){
+        min = node;
+        node = node->left;
+    }
+
+    return min;
+}
+
+void treeTransplant(struct Tree *tree, struct Node *node, struct Node *transplant){
+    if(transplant != NULL){
+        transplant->p = node->p;
+    }
+    
+    if(node->p == NULL){
+        tree->root = transplant;
+    }
+    else if(node->p->right == node){
+        node->p->right = transplant;
+    }
+    else{
+        node->p->left = transplant;
+    }
+}
+
+void treeRemove(struct Tree *tree, int num){
+    struct Node *remove;
+    
+    remove = treeSearch(tree, num);
+
+    if(remove == NULL)
+        return;
+
+    if(remove->left == NULL){
+        treeTransplant(tree, remove, remove->right);
+    }
+    else if(remove->right == NULL){
+        treeTransplant(tree, remove, remove->left);
+    }
+    else{
+        struct Node *successor;
+
+        successor = treeFindMin(remove);
+
+        if(successor != remove->right){
+            treeTransplant(tree, successor, successor->right);
+            successor->right = remove->right;
+            remove->right->p = successor;
+        }
+        
+        treeTransplant(tree, remove, successor);
+        successor->left = remove->left;
+        remove->left->p = successor;
+    }
+}
+
 void printAscendingHelper(struct Node *node){
     if(node == NULL)
         return;
@@ -84,6 +159,9 @@ int main(){
     treeInsert(&tree, 5);
     treeInsert(&tree, 6);
     treeInsert(&tree, 4);
+    treeRemove(&tree, 5);
+    treeRemove(&tree, 4);
+    treeRemove(&tree, 6);
 
     printAscending(&tree);
 }
